dfe7155490608ec3cda1d9a9abc5340603dc0917
Dan Nollette and Megan Olson <megan@>
2017-09-13 14:46:27 -0700

    add time tracker

--- lib/pet.rb
+++ lib/pet.rb
@@ -1,10 +1,11 @@
 class Pet
-  attr_accessor(:name, :food_level, :sleep_level, :activity_level)
+  attr_accessor(:name, :food_level, :sleep_level, :activity_level, :last_update)
   def initialize(name)
     @name = name
     @food_level = 10
     @sleep_level = 10
     @activity_level = 10
+    @last_update = Time.new
   end
   def is_alive?
     @food_level > 0
@@ -12,5 +13,39 @@ class Pet
   def time_passes
     @food_level -= 1
     @sleep_level -= 1
+  end
+  def update_pet
+    (((Time.new - @last_update).to_i)/15).times do
+      time_passes
+    end
+    # @last_update = Time.new
+  end
+  # def please_master
+  # end
+  def pet_status
+    if @food_level >= 8
+      food_string = "fed"
+    elsif @food_level >= 4
+      food_string = "hungry"
+    else
+      food_string = "starving"
+    end
+
+    if @sleep_level >= 8
+      sleep_string = "rested"
+    elsif @sleep_level >= 4
+      sleep_string = "kinda sleepy"
+    else
+      sleep_string = "plum tuckered"
+    end
+
+    if @activity_level >= 8
+      activity_string = "ready to roll"
+    elsif @activity_level >= 4
+      activity_string = "a little winded"
+    else
+      activity_string = "all worn out"
+    end
+    status_array = [food_string, sleep_string, activity_string]
   end
 end
--- spec/pet_spec.rb
+++ spec/pet_spec.rb
@@ -34,4 +34,39 @@ describe(Pet) do
       expect(my_pet.sleep_level()).to(eq(9))
     end
   end
+  describe("#pet_status") do
+    it("returns strings to tell how your pet is doing") do
+      my_pet = Pet.new("lil dragon")
+      expect(my_pet.pet_status()).to(eq(["fed", "rested", "ready to roll"]))
+    end
+    it("returns strings to tell how your pet is doing") do
+      my_pet = Pet.new("lil dragon")
+      my_pet.food_level = 2
+      my_pet.sleep_level = 2
+      my_pet.activity_level = 2
+      expect(my_pet.pet_status()).to(eq(["starving", "plum tuckered", "all worn out"]))
+    end
+    it("returns strings to tell how your pet is doing") do
+      my_pet = Pet.new("lil dragon")
+      my_pet.food_level = 5
+      my_pet.sleep_level = 5
+      my_pet.activity_level = 5
+      expect(my_pet.pet_status()).to(eq(["hungry", "kinda sleepy", "a little winded"]))
+    end
+  end
+  describe("#pet_status") do
+    it("compares the current time to the last time it was updated and changes your pet accordingly") do
+      my_pet = Pet.new("lil dragon")
+      my_pet.last_update= (my_pet.last_update - Time.at(60))
+      temp = my_pet.last_update
+      my_pet.update_pet
+      # expect(my_pet.food_level()).to(eq(6))
+      expect((my_pet.last_update - temp) > 59)
+    end
+  end
 end
+
+
+# (time_right_now - stored_time / 60).times do
+#   time_passes
+# end
